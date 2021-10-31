bdfb08f59d064b0b74ffb3aae6387f22a0a4d9fd
jegnaw2021 <jegnawjegnaw@gmail.com>
2021-10-31 15:36:40 +0300

    modified app.js to access a heroku backend server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import React, {useState} from "react"
 import './App.css';
+import Axios  from 'axios'
 
 function App() {
+
+  const [movieName, setMovieName] = useState('')
+  const [review, setReview] = useState('')
+
+  const submitReview = () => {
+    Axios.post("https://nrwl-server.herokuapp.com/api/insert",{
+      movieName: movieName,
+      movieReview: review,
+    }).then(() => {
+      alert('successful insert')
+    })
+  }
+
   return (
     <div className="App">
       <div className="movie_review_form">
@@ -11,16 +26,20 @@ function App() {
 
         <div>
           <label htmlFor="movie_name">Movie Name</label>
-          <input type="text" id="movie_name" name="movie_name" />
+          <input type="text" id="movie_name" name="movie_name" onChange={(e)=>{
+            setMovieName(e.target.value)
+          }}/>
         </div>
         
         <div>
           <label htmlFor="movie_review">Movie Review</label>
-          <input type="text" id="movie_review" name="movie_review" />
+          <input type="text" id="movie_review" name="movie_review" onChange={(e)=>{
+            setReview(e.target.value)
+          }}/>
         </div>
         
         <div>
-          <button>Submit</button>
+          <button onClick={submitReview}>Submit</button>
         </div>
       </div>
     </div>
