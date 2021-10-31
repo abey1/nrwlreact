8ee75f9e2e2218ff950a5d6bdbb380baf1be442f
jegnaw2021 <jegnawjegnaw@gmail.com>
2021-10-31 10:45:42 +0300

    modified app.js to display crud example movie name and description displaying form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hello
+      <div className="movie_review_form">
+
+        <h1>CRUD Application</h1>
+        <h4>soon to be nrwl project management system</h4>
+        <h5>remote mysql integration</h5>
+
+        <div>
+          <label htmlFor="movie_name">Movie Name</label>
+          <input type="text" id="movie_name" name="movie_name" />
+        </div>
+        
+        <div>
+          <label htmlFor="movie_review">Movie Review</label>
+          <input type="text" id="movie_review" name="movie_review" />
+        </div>
+        
+        <div>
+          <button>Submit</button>
+        </div>
+      </div>
     </div>
   );
 }
