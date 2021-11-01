7c129b8687c71c3c1e2908f2befb5855fbd59932
jegnaw2021 <jegnawjegnaw@gmail.com>
2021-11-01 10:17:53 +0300

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
 import React, {useState} from "react"
-import './App.css';
-import Axios  from 'axios'
-
+import './css/bootstrap.min.css'
 function App() {
-
-  const [movieName, setMovieName] = useState('')
-  const [review, setReview] = useState('')
-
-  const submitReview = () => {
-    Axios.post("https://nrwlpms.com/add_movie_review.php",{
-      movieName: movieName,
-      movieReview: review,
-    }).then(() => {
-      alert('successful insert')
-    })
-  }
-
   return (
     <div className="App">
-      <div className="movie_review_form">
-
-        <h1>CRUD Application</h1>
-        <h4>soon to be nrwl project management system</h4>
-        <h5>remote mysql integration</h5>
-
-        <div>
-          <label htmlFor="movie_name">Movie Name</label>
-          <input type="text" id="movie_name" name="movie_name" onChange={(e)=>{
-            setMovieName(e.target.value)
-          }}/>
-        </div>
-        
-        <div>
-          <label htmlFor="movie_review">Movie Review</label>
-          <input type="text" id="movie_review" name="movie_review" onChange={(e)=>{
-            setReview(e.target.value)
-          }}/>
-        </div>
-        
-        <div>
-          <button onClick={submitReview}>Submit</button>
-        </div>
+      <div className="container">
+        <p className="display-1">nrwl <kbd>project management system</kbd>  under construction...</p>
       </div>
     </div>
   );
